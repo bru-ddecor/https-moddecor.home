e12331b487d7dfeacff393e3c96dfbe59921e0c3
Bruna <bru.tironiol@gmail.com>
2025-04-04 06:20:54 -0300

    alteraçôes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Importa o Express
 const app = express(); // Inicia o servidor
-const port = 80; //porta do servidor
+const port = process.env.PORT || 3000; //porta do servidor
 // Servindo arquivos estáticos (HTML, CSS, JS, imagens)
 app.use(express.static("public")); // Pasta onde está seu site
 
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 
 // Inicia o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://moddecor.home:${port}`);
+    console.log(`Servidor rodando em http://moddecor.vercel.app:${port}`);
 });
